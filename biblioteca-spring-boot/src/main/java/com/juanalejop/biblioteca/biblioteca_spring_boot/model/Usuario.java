2ac36de21638f184d94d2fc291d825fa4a963fcf
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-05-14 15:56:52 -0300

    feature/models: Implementar clase Usuario

---
+++ biblioteca-spring-boot/src/main/java/com/juanalejop/biblioteca/biblioteca_spring_boot/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.juanalejop.biblioteca.biblioteca_spring_boot.model;
+
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+    private Boolean activo;
+
+    public Usuario() {}
+
+    public Usuario(Long id, String nombre, String email, Boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.activo = activo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", activo=" + activo +
+                '}';
+    }
+}
